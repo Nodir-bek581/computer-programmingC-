a16e44da616e8460bc68cca667f41e58cce0dc89
Nodir-bek581 <nodirbekodilov581@gmail.com>
2025-09-08 13:53:16 +0500

    week3 lab

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+// Problem 1
+/*int main() {
+    int intNumber = 24;
+    float floatNumber = 3.14;
+    double doubleNumber = 45.1234;
+    bool booleanNumber = true;
+    char charName = 'A';
+    cout << "Integer value is " << intNumber << ", the size is " << sizeof(intNumber) << endl;
+    cout << "Float value is " << floatNumber << ", the size is " << sizeof(floatNumber) << endl;
+    cout << "Double value is " << doubleNumber << ", the size is " << sizeof(doubleNumber) << endl;
+    cout << "Bool value is " << booleanNumber << ", the size is " << sizeof(booleanNumber) << endl;
+    cout << "Char value is " << charName << ", the size is " << sizeof(charName) << endl;
+} */
+
+//Problem 2
+/*int main() {
+    string myName = "Nodirbek Odilov";
+    int myID = 250153;
+    string mySong = "Shaxriyor - Agar dunyoni";
+    string myFavMov = "Avangers";
+    cout << myName << endl;
+    cout << myID << endl;
+    cout << mySong << endl;
+    cout << myFavMov << endl;
+    return 0;
+}*/
+
+//Problem 3
+/*
+int main() {
+    string text = "\"I\'m standing \" on the edge of some crazy cliff. What i have to do, , I have to catch everybody\\ if they start go over the cliff - I mean\\ if they\'re running and they don\'t look where they\'re going I have to come out from somewhere and   catch them";
+    cout << text << endl;
+} */
+
+//Problem 4
+/*
+int main() {
+    float a = 189.0;
+    char b = 'B';
+    int decimal_value_of_b = b;
+    float n1 = a/3;
+    float n2 = (a+decimal_value_of_b)/5;
+    char S = decimal_value_of_b + n2;
+    cout << n1 << " " << decimal_value_of_b << " " << n2 << " " << S << endl;
+}*/
+
+//Problem 5
+/*int main() {
+    int a = 12;
+    int b = 38;
+    int sum = a + b;
+    int product = a*b;
+    cout << "Sum=" << sum << endl;
+    cout << "Product=" << product << endl;
+}*/
+
+//Problem 6
+ /*int main() {
+     int x, y;
+     cin >> x >> y;
+     int sum = x + y;
+     cout << "Sum = " << sum << endl;
+ }*/
